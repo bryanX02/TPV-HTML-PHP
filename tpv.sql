3ad064d98fb4f7cf7f0bbc64b74044363fa73cc3
seiyo10 <seiyoser@gmail.com>
2022-03-04 11:16:46 +0100

    tpv actualizada

--- tpv.sql
+++ tpv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-03-2022 a las 10:44:34
+-- Tiempo de generación: 04-03-2022 a las 11:16:18
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
@@ -119,7 +119,8 @@ CREATE TABLE `mesas` (
 
 INSERT INTO `mesas` (`idMesa`, `numero`, `estado`) VALUES
 (1, 1, 0),
-(2, 1, 1);
+(2, 1, 1),
+(3, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -144,46 +145,46 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`idProducto`, `tipo`, `nombre`, `referencia`, `descripcion`, `stock`, `precio`, `iva`, `imagen`) VALUES
-(1, 1, 'COCACOLA', 'REF1', '', 19, 1.5, 21, ''),
-(2, 1, 'COCACOLA ZERO', 'REF2', '', 20, 1.5, 21, ''),
-(3, 1, 'COCACOLA PLUS COFFEE', 'REF3', '', 20, 1.8, 21, ''),
-(4, 1, 'FANTA NARANJA', 'REF4', '', 20, 1.5, 21, ''),
-(5, 1, 'FANTA LIMÓN', 'REF5', '', 20, 1.5, 21, ''),
-(6, 1, 'NESTEA', 'REF6', '', 20, 1.6, 21, ''),
-(7, 1, 'MOUNTAIN DEW', 'REF7', '', 20, 2, 21, ''),
-(8, 1, 'SPRITE', 'REF8', '', 20, 1.5, 21, ''),
-(9, 1, 'TINTO VERANO', 'REF9', '', 20, 2, 21, ''),
-(10, 1, 'VINO TINTO', 'REF10', '', 20, 7, 21, ''),
-(11, 1, 'AGUA CON GAS', 'REF11', '', 20, 1.8, 10, ''),
-(12, 1, 'AGUA', 'REF12', '', 20, 1, 10, ''),
-(13, 1, 'CERVEZA GARGANZUELA', 'REF13', '', 20, 2.5, 21, ''),
-(14, 1, 'CERVEZA ESTRELLA LEVANTE', 'REF14', '', 20, 1.2, 21, ''),
-(15, 1, 'HEINEKEN', 'REF15', '', 20, 3, 21, ''),
-(16, 1, 'CERVEZA AMSTEL', 'REF16', '', 20, 1.2, 21, ''),
-(17, 2, 'CRISPY CHICKEN WINGS', 'ENT1', '', 20, 5, 10, ''),
-(18, 2, 'TEQUES', 'ENT2', '', 20, 4.5, 10, ''),
-(19, 2, 'NACHORREO', 'ENT3', '', 20, 6, 10, ''),
-(20, 2, 'HUMMUS SAPIENS', 'ENT4', '', 20, 4.2, 10, ''),
-(21, 3, 'AVE CÉSAR', 'ENS1', '', 20, 8, 10, ''),
-(22, 3, 'THAI CHICKEN', 'ENS2', '', 20, 9, 10, ''),
-(23, 3, 'SANA HORIA', 'ENS3', '', 20, 7.5, 10, ''),
-(24, 4, 'PATATAS FRITAS', 'COM1', '', 20, 3, 10, ''),
-(25, 4, 'PATATAS GAJO', 'COM2', '', 20, 3, 10, ''),
-(26, 4, 'BATATAS', 'COM3', '', 20, 3.5, 10, ''),
-(27, 4, 'BACON & CHEESE', 'COM4', '', 20, 4.3, 10, ''),
-(28, 5, 'HAMBURGUESA \'LA GRETA\'', 'HAM1', '', 20, 9.75, 10, ''),
-(29, 5, 'HAMBURGUESA \'LA SMOKE\'', 'HAM2', '', 20, 8.5, 10, ''),
-(30, 5, 'HAMBURGUESA \'PIGMA\'', 'HAM3', '', 20, 9, 10, ''),
-(31, 5, 'HAMBURGUESA \'CHIPOTLE\'', 'HAM4', '', 20, 9.2, 10, ''),
-(32, 5, 'HAMBURGUESA \'POLLOTARIANA\'', 'HAM5', '', 20, 9, 10, ''),
-(33, 5, 'HAMBURGUESA \'KEVIN CHICK\'', 'HAM6', '', 20, 10, 10, ''),
-(34, 5, 'HAMBURGUESA \'LA KIKI\'', 'HAM7', '', 20, 9.2, 10, ''),
-(35, 5, 'HAMBURGUESA \'YANKEE\'', 'HAM8', '', 20, 8.5, 10, ''),
-(36, 5, 'HAMBURGUESA \'BIG VEGGIE\'', 'HAM9', '', 20, 8, 10, ''),
-(37, 6, 'TARTA DE QUESO', 'POS1', '', 20, 4, 10, ''),
-(38, 6, 'FROZEN', 'POS2', '', 20, 3.5, 10, ''),
-(39, 6, 'FROZEN BOMB', 'POS3', '', 20, 4, 10, ''),
-(40, 6, 'ICAKE', 'POS4', '', 20, 3.5, 10, '');
+(1, 1, 'COCACOLA', 'REF1', '', 19, 1.5, 21, 'refrescos/coca.png'),
+(2, 1, 'COCACOLA ZERO', 'REF2', '', 20, 1.5, 21, 'refrescos/coca0.png'),
+(3, 1, 'COCACOLA PLUS COFFEE', 'REF3', '', 20, 1.8, 21, 'refrescos/coffee.png'),
+(4, 1, 'FANTA NARANJA', 'REF4', '', 20, 1.5, 21, 'refrescos/naranja.png'),
+(5, 1, 'FANTA LIMÓN', 'REF5', '', 20, 1.5, 21, 'refrescos/limon.png'),
+(6, 1, 'NESTEA', 'REF6', '', 20, 1.6, 21, 'refrescos/nestea.png'),
+(7, 1, 'MOUNTAIN DEW', 'REF7', '', 20, 2, 21, 'refrescos/mountainDew.png'),
+(8, 1, 'SPRITE', 'REF8', '', 20, 1.5, 21, 'refrescos/sprite.png'),
+(9, 1, 'TINTO VERANO', 'REF9', '', 20, 2, 21, 'refrescos/tinto.png'),
+(10, 1, 'VINO TINTO', 'REF10', '', 20, 7, 21, 'refrescos/vino.png'),
+(11, 1, 'AGUA CON GAS', 'REF11', '', 20, 1.8, 10, 'refrescos/vichycatalan.png'),
+(12, 1, 'AGUA', 'REF12', '', 20, 1, 10, 'refrescos/agua.png'),
+(13, 1, 'CERVEZA GARGANZUELA', 'REF13', '', 20, 2.5, 21, 'refrescos/cerveza.png'),
+(14, 1, 'CERVEZA ESTRELLA LEVANTE', 'REF14', '', 20, 1.2, 21, 'refrescos/estrella.png'),
+(15, 1, 'HEINEKEN', 'REF15', '', 20, 3, 21, 'refrescos/heineken.png'),
+(16, 1, 'CERVEZA AMSTEL', 'REF16', '', 20, 1.2, 21, 'refrescos/amster.png'),
+(17, 2, 'CRISPY CHICKEN WINGS', 'ENT1', '', 20, 5, 10, 'entrantes/crispy.png'),
+(18, 2, 'TEQUES', 'ENT2', '', 20, 4.5, 10, 'entrantes/teques.png'),
+(19, 2, 'NACHORREO', 'ENT3', '', 20, 6, 10, 'entrantes/nachos.png'),
+(20, 2, 'HUMMUS SAPIENS', 'ENT4', '', 20, 4.2, 10, 'entrantes/hummus.png'),
+(21, 3, 'AVE CÉSAR', 'ENS1', '', 20, 8, 10, 'ensaladas/cesar.PNG'),
+(22, 3, 'THAI CHICKEN', 'ENS2', '', 20, 9, 10, 'ensaladas/thai.PNG'),
+(23, 3, 'SANA HORIA', 'ENS3', '', 20, 7.5, 10, 'ensaladas/sanahoria.PNG'),
+(24, 4, 'PATATAS FRITAS', 'COM1', '', 20, 3, 10, 'complementos/patatas.png'),
+(25, 4, 'PATATAS GAJO', 'COM2', '', 20, 3, 10, 'complementos/gajos.png'),
+(26, 4, 'BATATAS', 'COM3', '', 20, 3.5, 10, 'complementos/batata.png'),
+(27, 4, 'BACON & CHEESE', 'COM4', '', 20, 4.3, 10, 'complementos/bachoncheese.png'),
+(28, 5, 'HAMBURGUESA \'LA GRETA\'', 'HAM1', '', 20, 9.75, 10, 'hamburguesas/greta.PNG'),
+(29, 5, 'HAMBURGUESA \'LA SMOKE\'', 'HAM2', '', 20, 8.5, 10, 'hamburguesas/smoke.PNG'),
+(30, 5, 'HAMBURGUESA \'PIGMA\'', 'HAM3', '', 20, 9, 10, 'hamburguesas/pigma.PNG'),
+(31, 5, 'HAMBURGUESA \'CHIPOTLE\'', 'HAM4', '', 20, 9.2, 10, 'hamburguesas/chipotle.PNG'),
+(32, 5, 'HAMBURGUESA \'POLLOTARIANA\'', 'HAM5', '', 20, 9, 10, 'hamburguesas/pollotariana.PNG'),
+(33, 5, 'HAMBURGUESA \'KEVIN CHICK\'', 'HAM6', '', 20, 10, 10, 'hamburguesas/kevin.PNG'),
+(34, 5, 'HAMBURGUESA \'LA KIKI\'', 'HAM7', '', 20, 9.2, 10, 'hamburguesas/kiki.PNG'),
+(35, 5, 'HAMBURGUESA \'YANKEE\'', 'HAM8', '', 20, 8.5, 10, 'hamburguesas/yankee.PNG'),
+(36, 5, 'HAMBURGUESA \'EDAMAMI\'', 'HAM9', '', 20, 8, 10, 'hamburguesas/edamami.PNG'),
+(37, 6, 'TARTA DE QUESO', 'POS1', '', 20, 4, 10, 'postres/queso.png'),
+(38, 6, 'FROZEN', 'POS2', '', 20, 3.5, 10, 'postres/frozen.png'),
+(39, 6, 'FROZEN BOMB', 'POS3', '', 20, 4, 10, 'postres/bombPNG.PNG'),
+(40, 6, 'ICAKE', 'POS4', '', 20, 3.5, 10, 'postres/icake.PNG');
 
 --
 -- Índices para tablas volcadas
@@ -257,7 +258,7 @@ ALTER TABLE `lineaspedidos`
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `idMesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idMesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
